74dc6ed9f2861aaa1d865d181019b36756a623e7
FavoursGit <favourealist@gmail.com>
2025-03-07 13:01:58 +0000

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>King Kicks</title>
+    <link rel="stylesheet" href="css/home.css">
+    <script src="js/home.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+    <header>
+        <section class="nav">
+            <ul>
+                <li><a href="homepage.php"><img src="images/White_KingKicks.png" width="60" height="50"></a></li>
+                <li class="dropdown">
+                    <a href="brands.php">Brands</a>
+                    <div class="dropdown-content">
+                        <a href="Nike.php">Nike</a>
+                        <a href="Jordan.php">Jordan</a>
+                        <a href="NewBalance.php">New Balance</a>
+                        <a href="Yeezys.php">Yeezys</a>
+                    </div>
+                </li>
+                </li>
+                <li><a href="all.php">Shop All</a></li>
+                <li><a href="new.php">New</a></li>
+                <li><a href="sale.php">Sale</a></li>
+                <li><input type="text" placeholder="Search"></li>
+                <li><a href="signup.php"><img src="images/login.png" width="50" height="50"></a></li>
+                <li><a href="cart.php"><img src="images/cart.png" width="50" height="50"></li>
+            </ul>
+        </section>
+    </header>
+
+    <?php
+        $servername = "localhost";
+        $username = "root"; 
+        $password = "";
+        $dbname = "database";
+
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Connection failed: " . $mysqli_connect_error());
+    }
+    else
+    {
+        echo "Connected successfully";
+    }
+
+    $sql ="SELECT * FROM products";
+
+    $QRYresult = mysqli_query($conn, $sql);
+    while($row = mysqli_fetch_array($QRYresult))
+    {
+        echo "<br>";
+        echo $row['PRODUCT_ID']."<br>";
+        echo $row['PRODUCT_NAME']."<br>";
+        echo $row['BRAND']."<br>";
+        echo $row['PRICE']."<br>";
+        echo $row['AMOUNTORDERED']."<br>";
+        echo $row['STOCK']."<br>";
+    }
+    mysqli_close($conn);
+    ?>
+    <footer>
+        <div class="footer-content">
+            <ul>
+                <h3>Contact Us</h3>
+                <p>Email: KingKicks@Gmail.com</p>
+                <p>Phone: 123-456-7890</p>
+                <p>Address: 123 KingKicks Lane</p>
+            </ul>
+        </div>
+        <div class="footer-content">
+            <ul>
+                <h3>Founders</h3>
+                <p>Favour Godson</p>
+                <p>Luke Douglas</p>
+                <p>Nathan Lynch</p>
+            </ul>
+        </div>
+        <div class="footer-content">
+            <ul>
+                <h3>Follow Us</h3>
+                <li class="social-icons">
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com"><i class="fab fa-linkedin"></i></a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
